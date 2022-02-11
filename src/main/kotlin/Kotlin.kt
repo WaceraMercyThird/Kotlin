11cfe2e3443bf40b518051e975cfd5c4d2d767a7
WaceraMercyThird <waceraaamercy@gmail.com>
2022-02-11 22:55:53 +0300

    add Kotlin.kt

---
+++ src/main/kotlin/Kotlin.kt
@@ -0,0 +1,72 @@
+fun main() {
+
+    var nams = arrayOf("JANE WAIGURU", "ANN MUMGAI", "RUTH WANJA", "SUM KIMANI", "KIM KANYE" )
+    var age = arrayOf( "23", "19", "20", "34", "67")
+    var pnumber = arrayOf("0734254356", "074675487", "07454679897", "0786879878")
+    var weight = arrayOf("45.4kg", "56.3kg", "67kg", "40kg")
+
+//    First student record...
+    println(nams[0])
+    println(age[0])
+    println(pnumber[0])
+    println(weight[0])
+// Nationality of the student...
+    val s0weight = 45.4
+    if (s0weight < 78) {
+        println("kenyan")
+    } else{
+        println("not kenyan")
+    }
+
+
+//    Second student record....
+    println(nams[1])
+    println(age[1])
+    println(pnumber[1])
+    println(weight[1])
+
+// Nationality of the student...
+    val i = 45
+
+    if (i > 78) {
+        println("kenyan")
+    } else{
+        println("not kenyan")
+    }
+
+    //    Third student record....
+    println(nams[2])
+    println(age[2])
+    println(pnumber[2])
+    println(weight[2])
+
+// Nationality of the student...
+    val s2weight = 45
+
+    if ( s2weight > 78) {
+        println("kenyan")
+    } else{
+        println("not kenyan")
+    }
+    //    Fourth student record....
+    println(nams[3])
+    println(age[3])
+    println(pnumber[3])
+    println(weight[3])
+
+// Nationality of the student...
+    val s3weight = 45
+
+    if ( s3weight > 20) {
+        println("kenyan")
+    } else{
+        println("not kenyan")
+    }
+
+
+
+
+
+
+
+}
