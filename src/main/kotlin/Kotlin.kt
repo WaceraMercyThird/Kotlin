38f91490616ab17ce94dbcf8d938ff5d9bfe433d
WaceraMercyThird <99390165+WaceraMercyThird@users.noreply.github.com>
2022-02-12 09:12:15 +0300

    Update Kotlin.kt

--- src/main/kotlin/Kotlin.kt
+++ src/main/kotlin/Kotlin.kt
@@ -4,19 +4,14 @@ fun main() {
     var age = arrayOf( "23", "19", "20", "34", "67")
     var pnumber = arrayOf("0734254356", "074675487", "07454679897", "0786879878")
     var weight = arrayOf("45.4kg", "56.3kg", "67kg", "40kg")
+    var Kenyan = arrayOf("true","false", "true", "false")
 
 //    First student record...
     println(names[0])
     println(age[0])
     println(pnumber[0])
     println(weight[0])
-// Nationality of the student...
-    val s0weight = 45.4
-    if (s0weight < 78) {
-        println("kenyan")
-    } else{
-        println("not kenyan")
-    }
+    println(Kenyan[0])
 
 
 //    Second student record....
@@ -24,49 +19,20 @@ fun main() {
     println(age[1])
     println(pnumber[1])
     println(weight[1])
-
-// Nationality of the student...
-    val i = 45
-
-    if (i > 78) {
-        println("kenyan")
-    } else{
-        println("not kenyan")
-    }
+    println(Kenyan[1])
 
     //    Third student record....
     println(names[2])
     println(age[2])
     println(pnumber[2])
     println(weight[2])
+    println(Kenyan[2])
+     
 
-// Nationality of the student...
-    val s2weight = 45
-
-    if ( s2weight > 78) {
-        println("kenyan")
-    } else{
-        println("not kenyan")
-    }
     //    Fourth student record....
     println(names[3])
     println(age[3])
     println(pnumber[3])
     println(weight[3])
-
-// Nationality of the student...
-    val s3weight = 45
-
-    if ( s3weight > 20) {
-        println("kenyan")
-    } else{
-        println("not kenyan")
-    }
-
-
-
-
-
-
-
+    println(Kenyan[3])
 }
