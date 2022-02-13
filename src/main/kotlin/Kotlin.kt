8fda6b007bb816f46cfcf096bd1d24a839076f04
WaceraMercyEsther <99390165+WaceraMercyThird@users.noreply.github.com>
2022-02-13 14:33:28 +0300

    Update Kotlin.kt

--- src/main/kotlin/Kotlin.kt
+++ src/main/kotlin/Kotlin.kt
@@ -1,17 +1,18 @@
 fun main() {
 
     var names = arrayOf("JANE WAIGURU", "ANN MUMGAI", "RUTH WANJA", "SUM KIMANI", "KIM KANYE" )
-    var age = arrayOf( "23", "19", "20", "34", "67")
+    var age = arrayOf( "23 yrs old", "19 yrs old", "20 yrs old", "34 yrs old", "67 yrs old")
     var pnumber = arrayOf("0734254356", "074675487", "07454679897", "0786879878")
     var weight = arrayOf("45.4kg", "56.3kg", "67kg", "40kg")
-    var Kenyan = arrayOf("true","false", "true", "false")
+    var Kenyan = arrayOf("True", "False", "True", "False")
 
 //    First student record...
     println(names[0])
     println(age[0])
     println(pnumber[0])
     println(weight[0])
-    println(Kenyan[0])
+    println("")
+
 
 
 //    Second student record....
@@ -20,6 +21,7 @@ fun main() {
     println(pnumber[1])
     println(weight[1])
     println(Kenyan[1])
+    println("")
 
     //    Third student record....
     println(names[2])
@@ -27,7 +29,7 @@ fun main() {
     println(pnumber[2])
     println(weight[2])
     println(Kenyan[2])
-     
+    println("")
 
     //    Fourth student record....
     println(names[3])
@@ -35,4 +37,6 @@ fun main() {
     println(pnumber[3])
     println(weight[3])
     println(Kenyan[3])
+    println("")
+
 }
